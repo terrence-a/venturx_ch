c09b707de275c0bad8d5125795ecbf8ba0337cc3
Terrence Amponsah <terrence.amponsah@mail.utoronto.ca>
2022-01-19 14:27:58 -0800

    files for docker and default route implementation

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 'use strict';
 
 const express = require('express');
-const PORT = 3000;
+const PORT = 8080;
 const app = express();
 
+// The requested "/greetings/:name" route. 
 app.get('/greetings/:name', (req, res)=> {
     res.send(`Hello, ${req.params.name}`)
+})
+
+//default route. All routes except "/greetings/:name" go to here.
+app.use(function(req, res){
+    res.status(404).send("Invalid Route")
 })
 
 app.listen(PORT, () => {
